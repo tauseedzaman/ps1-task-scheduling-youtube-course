750c7aaae141ba421844603355408eef8a88db81
tauseedzaman <tauseedzamanofsorana@gmail.com>
2023-06-22 13:22:26 +0100

    second video script added

---
+++ first_task.sp1.ps1
@@ -0,0 +1,19 @@
+﻿$ServiceName="WebClient"
+
+$TaskName="OurSecondTask"
+
+$user = "$(hostname)\$env:USERNAME"
+
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-Command start-service $ServiceName"
+
+$tasktrigger= New-ScheduledTaskTrigger -Once -At (Get-Date).AddMinutes(1)
+
+$principle= New-ScheduledTaskPrincipal -UserId $user -RunLevel Highest
+
+$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries
+
+$task = New-ScheduledTask -Action $action -Trigger $tasktrigger -Principal $principle -Settings $settings
+
+Register-ScheduledTask -TaskName  $TaskName -InputObject $task -Force
+
+write-host "$TaskName is registered"
